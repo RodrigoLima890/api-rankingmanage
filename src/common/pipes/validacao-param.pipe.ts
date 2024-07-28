faf391d546014e3668a4c909f224f0e173dabead
RodrigoLima890 <rodrigodaschagas1234@gmail.com>
2024-07-28 18:52:25 -0300

    Criando uma forma de tratar exceptions de forma global

--- src/common/pipes/validacao-param.pipe.ts
+++ src/common/pipes/validacao-param.pipe.ts
@@ -1,6 +1,6 @@
 import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
 
-export class JogadoresValidacaoParamPipe implements PipeTransform{
+export class ValidacaoParamPipe implements PipeTransform{
     transform(value: any, metadata: ArgumentMetadata) {
         if(!value) throw new BadRequestException(`O valor do parametro ${metadata.data} deve ser informado`); 
         return value;
--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller,Delete,Get,Param,Post, Put, UsePipes, ValidationPipe }
 import { CriarJogadorDTO } from './dtos/criar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
 import { Jogador } from './interfaces/jogador.interface';
-import { JogadoresValidacaoParamPipe } from './pipes/jogadores-validacao-param.pipe';
+import { ValidacaoParamPipe } from '../common/pipes/validacao-param.pipe';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -12,7 +12,7 @@ export class JogadoresController {
     @Put('/:_id')
     @UsePipes(ValidationPipe)
     async atualizarJogador(
-        @Body() jogadorDto:CriarJogadorDTO,@Param('_id', JogadoresValidacaoParamPipe) _id:string
+        @Body() jogadorDto:CriarJogadorDTO,@Param('_id', ValidacaoParamPipe) _id:string
     ):Promise<Object>{
         return await this.jogadoresService.atualizarJogador(_id,jogadorDto)
     }
@@ -26,7 +26,7 @@ export class JogadoresController {
     }
 
     @Get('/:_id')
-    async buscarJogadorPeloId(@Param('_id',JogadoresValidacaoParamPipe) _id:string):Promise<Jogador>{
+    async buscarJogadorPeloId(@Param('_id',ValidacaoParamPipe) _id:string):Promise<Jogador>{
         return await this.jogadoresService.buscarJogadorPeloId(_id);
     }
 
@@ -36,7 +36,7 @@ export class JogadoresController {
     }
 
     @Delete('/:_id')
-    async deletarJogadorPorid(@Param('_id', JogadoresValidacaoParamPipe) _id:string):Promise<Object>{
+    async deletarJogadorPorid(@Param('_id', ValidacaoParamPipe) _id:string):Promise<Object>{
         return await this.jogadoresService.deletarJogadorPorid(_id);
     }
 }
