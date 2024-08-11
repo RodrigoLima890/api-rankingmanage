313cedf08bac9f1a2d39d7a4ebbc5e8702cee5d6
RodrigoLima890 <rodrigodaschagas1234@gmail.com>
2024-08-11 11:44:58 -0300

    Feat: Criação do desafio validation status pipe

---
+++ src/desafios/pipes/desafio-validation-status.pipe.ts
@@ -0,0 +1,24 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { Desafios } from "../interfaces/desafios.interface";
+import { StatusDesafio } from "../enums/status-desafio.enum";
+
+export class DesafioValidationStatusPipe implements PipeTransform{
+    readonly statusPermitidos = [
+        StatusDesafio.ACEITO,
+        StatusDesafio.RECUSADO,
+        StatusDesafio.CANCELADO
+    ]
+    transform(value: Desafios) {
+        const status = value.status.toUpperCase();
+
+        if(this.statusValido(status)) throw new BadRequestException(`${status} não é um status válido`);
+        return value;
+    }
+
+    private statusValido(status: any){
+        const idx = this.statusPermitidos.indexOf(status);
+
+        return idx !== -1;
+    }
+    
+}
